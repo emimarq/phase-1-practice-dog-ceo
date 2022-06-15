86206e070261efc145e58b15d75ae11681f6ee35
Emilio <marqetm@gmail.com>
2022-06-15 17:53:54 -0500

    commit tree 46f2dbfeafd9f0e92df8003b2ca80b6f1851eaf4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedList = document.querySelector('select')
+const options = document.querySelector('option')
+const imgContainer = document.querySelector('#dog-image-container')
+//Dom render funtions
+function renderDogImgs() {
+
+}
+
+//Initialize fetch
+function initialize() {
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(dog => {
+      dog.message.forEach(dog => {
+        
+      });
+    }).catch(error => {
+      //Sends error message to DOM
+      alert('Could not find requested data.')
+    })
+}
+
+document.addEventListener('DOMContentLoaded', initialize)
